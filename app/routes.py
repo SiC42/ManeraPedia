50af167bc0a52e5914d6256737a2d8eda65c8e6d
Simon Bordewisch <sbordewisch@gmx.de>
2019-09-09 15:58:55 +0200

    Add site-map for debug purposes

--- app/routes.py
+++ app/routes.py
@@ -86,3 +86,24 @@ def protected():
 def logout():
     flask_login.logout_user()
     return 'Logged out'
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    links_html = "<ul>"
+    for link in links:
+        links_html += '<li><a href="{}">{}</a></li>'.format(link[0], link[1])
+    links_html += "</ul>"
+    return links_html
