9f3398cf48e9f484a6e1dcb0eca746b09114b85f
Simon Bordewisch <sbordewisch@gmx.de>
2019-10-20 19:20:04 +0200

    Change data handling

--- frontend/src/services/login.service.js
+++ frontend/src/services/login.service.js
@@ -1,40 +1,41 @@
-import config from 'config';
-import axios from 'axios';
+import axios from "axios";
+import config from "config";
 
 export const loginService = {
-    login,
-    logout,
-    //TODO: register,
+  login,
+  logout
+  //TODO: register,
 };
 
 function login(username, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    };
-    const body = JSON.stringify({ username, password });
-    return axios.post(`${config.apiUrl}/login`, body, requestOptions)
-        .then(handleResponse)
-        .then(user => {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('user', JSON.stringify(user));
-            return user.info;
-        })
-        .catch(handleError);
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" }
+  };
+  const body = JSON.stringify({ username, password });
+  return axios
+    .post(`${config.apiUrl}/login`, body, requestOptions)
+    .then(handleResponse)
+    .then(user => {
+      // store user details and jwt token in local storage to keep user logged in between page refreshes
+      localStorage.setItem("user", JSON.stringify(user));
+      return user;
+    })
+    .catch(handleError);
 }
 
 function logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('user');
+  // remove user from local storage to log user out
+  localStorage.removeItem("user");
 }
 
 function handleError(error) {
-    if(error.response) {
-        console.log(error.toJSON())
-        return Promise.reject(error.toJSON());
-    }
+  if (error.response) {
+    console.log(error.toJSON());
+    return Promise.reject(error.toJSON());
+  }
 }
 
 function handleResponse(response) {
-    return response.data;
+  return response.data;
 }
