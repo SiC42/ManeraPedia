dc0138cf5214938905d0c494984bf29bcf8ff588
Simon Bordewisch <sbordewisch@gmx.de>
2019-10-20 19:22:39 +0200

    Change behavior based on logged in and logging in

--- frontend/src/components/menues/login_dialog.jsx
+++ frontend/src/components/menues/login_dialog.jsx
@@ -1,53 +1,67 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Dialog } from '@material-ui/core';
-import {loginActions} from "actions"
-import { useDispatch } from "react-redux";
-
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Dialog } from "@material-ui/core";
+import { loginActions } from "actions";
+import { useDispatch, useSelector } from "react-redux";
+import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
+  "@global": {
     body: {
-      backgroundColor: theme.palette.common.white,
-    },
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  buttonProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
   },
   paper: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(2, 0, 2)
+  }
 }));
 export default function LoginDialog(props) {
-  const {handleClose, open} = props
+  const { handleClose, open } = props;
   const classes = useStyles();
   const [username, setUserName] = React.useState("");
   const [password, setPassword] = React.useState("");
+  
+  const loggingIn = useSelector(state => state.login.loggingIn);
 
   const dispatch = useDispatch();
   const login = () => {
     dispatch(loginActions.login(username, password));
-  }
-
+  };
 
   return (
     <div>
@@ -56,53 +70,67 @@ export default function LoginDialog(props) {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <CssBaseline/>
+        <CssBaseline />
         <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={e => e.preventDefault()}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            label="User Name"
-            name="username"
-            value={username}
-            onChange={e=> setUserName(e.target.value)}
-            autoComplete="username"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            value={password}
-            onChange={e=> setPassword(e.target.value)}
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={() => login()}
-          >
-            LogIn
-          </Button>
-        </form>
-      </div>
-      </Container>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <form
+              className={classes.form}
+              noValidate
+              onSubmit={e => e.preventDefault()}
+            >
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="User Name"
+                name="username"
+                value={username}
+                onChange={e => setUserName(e.target.value)}
+                autoComplete="username"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                autoComplete="current-password"
+              />
+              <div className={classes.wrapper}>
+                <Button
+                  type="button"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={() => login()}
+                  disabled={loggingIn}
+                >
+                  Login
+                </Button>
+                {
+                  loggingIn &&
+                  <CircularProgress
+                    size={24}
+                    className={classes.buttonProgress}
+                  />
+                }
+              </div>
+            </form>
+          </div>
+        </Container>
       </Dialog>
     </div>
   );
