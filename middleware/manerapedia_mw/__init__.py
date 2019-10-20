4105a36046ea680553e02b46efb75d0ae806f485
Simon Bordewisch <sbordewisch@gmx.de>
2019-10-20 16:22:50 +0200

    Add CORS-handling

--- middleware/manerapedia_mw/__init__.py
+++ middleware/manerapedia_mw/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, session
+from flask_cors import CORS
 
 web_api = Flask(__name__)
+web_api.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(web_api)
 
 # Changes this later to something that is not in a repository
 web_api.secret_key = b'\x9dtB\xdd\x81b\xd7\xbc\x87\x12i\xd4\xd9\xf3\x95k'
