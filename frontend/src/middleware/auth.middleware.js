7685fb7a37cedce320b1e89a452a3893222566d7
Simon Bordewisch <sbordewisch@gmx.de>
2019-10-26 11:40:56 +0200

    Refactor auth middleware to use action properties instead of types

--- frontend/src/middleware/auth.middleware.js
+++ frontend/src/middleware/auth.middleware.js
@@ -1,49 +1,61 @@
-import jwt_decode from "jwt-decode";
 import { authOperations, authTypes } from "ducks/auth";
-import { searchTypes } from "ducks/search";
-import { getAuthHeader } from "../helpers";
 import { loginService } from "../services/auth.service";
+import {
+  ACCESS_TOKEN,
+  authNeeded,
+  getAuthMetaType,
+  getAuthHeader,
+  REFRESH_TOKEN
+} from "helpers/auth";
+import jwt_decode from "jwt-decode";
 
 let buffer = [];
-const authNeededActionTypes = [searchTypes.AUTOCOMPLETE_REQUEST];
 
 export const authMiddleware = ({ dispatch, getState }) => next => action => {
-  let state = getState();
-  if (action.type === authTypes.REFRESH_TOKEN) {
-    next(actionWithHeader(action, state.login.refresh_token));
-  }
+  // Catch specific actions
   if (action.type === authTypes.REFRESH_TOKEN_SUCCESS) {
-    buffer.forEach(a => next(actionWithHeader(a, action.payload)));
+    buffer.forEach(a => next(actionWithAuthHeader(a, action.payload)));
     buffer = [];
   }
   if (action.type === authTypes.LOGOUT) {
     loginService.logout();
+
+  // Pass on actions that do not need any kind of authentication(-header)
+  if (!authNeeded(action)) {
+    return next(action);
   }
 
-  if (authNeededActionTypes.includes(action.type)) {
+  let state = getState();
+
+  // Catch actions which need the refresh token
+  if (getAuthMetaType(action) === REFRESH_TOKEN) {
+    next(actionWithAuthHeader(action, state.login.refresh_token));
+  }
+
+  // Catch actions which need the access token
+  if (getAuthMetaType(action) === ACCESS_TOKEN) {
+    if (!state.login) {
+      // We are not logged in
+      return next(action);
+    }
     if (tokenExpired(state)) {
       buffer.push(action);
       dispatch(authOperations.refreshToken(state.login.refresh_token));
     } else {
-      return next(actionWithHeader(action, state.login.access_token));
+      return next(actionWithAuthHeader(action, state.login.access_token));
     }
   }
-  return next(action);
 };
 
 function tokenExpired(state) {
-  if (state.login && state.login.access_token && state.login.refresh_token) {
-    // decode jwt so that we know if and when it expires
-    const tokenExpiration = jwt_decode(state.login.access_token).exp;
-    const nowInSeconds = Math.floor(Date.now() / 1000);
-    return tokenExpiration && tokenExpiration - nowInSeconds < 10;
-  } else {
-    return false;
-  }
+  // decode jwt so that we know if and when it expires
+  const tokenExpiration = jwt_decode(state.login.access_token).exp;
+  const nowInSeconds = Math.floor(Date.now() / 1000);
+  return tokenExpiration && tokenExpiration - nowInSeconds < 10;
 }
 
-function actionWithHeader(action, access_token) {
+function actionWithAuthHeader(action, token) {
   const actionWithAuthHeader = Object.assign({}, action);
-  actionWithAuthHeader.payload.Authorization = getAuthHeader(access_token);
+  actionWithAuthHeader.payload.Authorization = getAuthHeader(token);
   return actionWithAuthHeader;
 }
