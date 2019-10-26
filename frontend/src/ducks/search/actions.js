79bac2169c5dfdae076b3426439f425ab61182de
Simon Bordewisch <sbordewisch@gmx.de>
2019-10-26 11:45:31 +0200

    Restructure actions to incorporate meta data for auth needed

--- frontend/src/ducks/search/actions.js
+++ frontend/src/ducks/search/actions.js
@@ -1,9 +1,14 @@
 import * as types from "./types";
+import { ACCESS_TOKEN, addAuthNeededMeta } from "helpers/auth";
 
 export { autocompleteRequest, autocompleteFailure, autocompleteSuccess };
 
 function autocompleteRequest(phrase) {
-  return { type: types.AUTOCOMPLETE_REQUEST, payload: { phrase } };
+  return {
+    type: types.AUTOCOMPLETE_REQUEST,
+    payload: { phrase },
+    meta: addAuthNeededMeta(ACCESS_TOKEN)
+  };
 }
 function autocompleteSuccess(suggestions) {
   return { type: types.AUTOCOMPLETE_SUCCESS, payload: suggestions };
