9b0bab7844830c2ece07a33476e7521a7393c827
Simon Bordewisch <sbordewisch@gmx.de>
2019-10-27 14:35:44 +0100

    Remove console log

--- frontend/src/ducks/tab/reducers.js
+++ frontend/src/ducks/tab/reducers.js
@@ -5,7 +5,6 @@ export default function tabs(state = [], action) {
     case types.ADD_TAB:
       return [...state, action.payload];
     case types.REMOVE_TAB:
-      console.log(action.index);
       return state.filter((e, i) => i !== action.payload);
     default:
       return state;
