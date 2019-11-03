93b6f25109e8ecc7bc5dc81f7f42f4f58924fad8
Simon Bordewisch <sbordewisch@gmx.de>
2019-11-03 15:55:23 +0100

    Replace Downshift with hook

--- frontend/src/components/menues/header/search.header.jsx
+++ frontend/src/components/menues/header/search.header.jsx
@@ -1,14 +1,10 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable react/jsx-props-no-spreading */ // As it is a useful feature in Downshift
-import { Popper } from "@material-ui/core";
 import InputBase from "@material-ui/core/InputBase";
 import MenuItem from "@material-ui/core/MenuItem";
-import Paper from "@material-ui/core/Paper";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
-import Downshift from "downshift";
-import React from "react";
 import { searchOperations } from "ducks/search/";
+import useAutocomplete from "hooks/useAutocomplete";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const useStyles = makeStyles(theme => ({
@@ -62,60 +58,15 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-let popperNode;
 export default function Search() {
   const classes = useStyles();
   const dispatch = useDispatch();
-
-  function renderSuggestion(suggestionProps) {
-    const { suggestion, index, itemProps, highlightedIndex } = suggestionProps;
-    const isHighlighted = highlightedIndex === index;
-    return (
-      <MenuItem
-        {...itemProps}
-        key={suggestion.id}
-        selected={isHighlighted}
-        component="div"
-      >
-        {suggestion.title}
-      </MenuItem>
-    );
-  }
 
   const suggestions = useSelector(state =>
     state.search.suggestions ? state.search.suggestions : []
   );
 
-  function downshiftReducer(state, changes) {
-    console.log(state);
-    console.log(changes);
-    switch (changes.type) {
-      case Downshift.stateChangeTypes.keyDownArrowUp:
-        if (state.highlightedIndex === 0) {
-          return { ...changes, highlightedIndex: null };
-        }
-        return changes;
-      case Downshift.stateChangeTypes.keyDownArrowDown:
-        if (state.highlightedIndex === suggestions.length) {
-          return { ...changes, highlightedIndex: null };
-        }
-        return changes;
-
-      case Downshift.stateChangeTypes.keyDownEnter:
-      case Downshift.stateChangeTypes.clickItem:
-        return {
-          ...changes,
-          inputValue: ""
-        };
-      default:
-        return changes;
-    }
-  }
-
   const fetchSuggestions = event => {
-    if (!event.target.value) {
-      return;
-    }
     dispatch(searchOperations.autocompleteRequest(event.target.value));
   };
 
@@ -123,76 +74,42 @@ export default function Search() {
     dispatch(searchOperations.getArticleRequest(selection));
   };
 
-  const handleEnter = reset => event => {
-    if (event.key === "Enter") {
-      console.log("Searching");
-      reset({ type: Downshift.stateChangeTypes.keyDownEnter });
-    }
+  const { input, inputProps, Popper, selectedItem } = useAutocomplete({
+    suggestions,
+    onEnterFunction: fetchSelectedArticle,
+    fetchSuggestionsFunction: fetchSuggestions
+  });
+
+  const renderInput = () => {
+    return (
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...inputProps}
+          margin="none"
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput
+          }}
+        />
+        <Popper placement="bottom-end" className={classes.popper}>
+          <MenuItem
+            key="searchContainsMenuItem"
+            selected={selectedItem === suggestions.length}
+            component="div"
+          >
+            <i>containing... </i>
+            &nbsp;
+            {` ${input}`}
+          </MenuItem>
+        </Popper>
+      </div>
+    );
   };
 
-  return (
-    <Downshift onChange={fetchSelectedArticle} stateReducer={downshiftReducer}>
-      {({
-        getInputProps,
-        getItemProps,
-        getMenuProps,
-        isOpen,
-        highlightedIndex,
-        reset
-      }) => {
-        return (
-          <div>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                onKeyPress={handleEnter(reset)}
-                inputRef={node => {
-                  popperNode = node;
-                }}
-                inputProps={getInputProps({
-                  onChange: fetchSuggestions
-                })}
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-              />
-            </div>
-            <Popper
-              open={isOpen}
-              anchorEl={popperNode}
-              style={{ zIndex: 1300 }}
-            >
-              <div
-                {...(isOpen
-                  ? getMenuProps({}, { suppressRefError: true })
-                  : {})}
-              >
-                <Paper
-                  className={classes.paper}
-                  square
-                  style={{
-                    width: popperNode ? popperNode.clientWidth : undefined
-                  }}
-                >
-                  {suggestions.map((suggestion, index) =>
-                    renderSuggestion({
-                      suggestion,
-                      index,
-                      itemProps: getItemProps({
-                        item: suggestion.title
-                      }),
-                      highlightedIndex
-                    })
-                  )}
-                </Paper>
-              </div>
-            </Popper>
-          </div>
-        );
-      }}
-    </Downshift>
-  );
+  return renderInput();
 }
