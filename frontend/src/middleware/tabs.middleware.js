ae40092362a208af54cc7bd546b0e8e0a0bf98d1
Simon Bordewisch <sbordewisch@gmx.de>
2019-11-03 18:59:53 +0100

    Update tabs.middleware.js

--- frontend/src/middleware/tabs.middleware.js
+++ frontend/src/middleware/tabs.middleware.js
@@ -2,7 +2,6 @@ import { tabOperations, tabTypes } from "ducks/tabs";
 
 export default ({ getState }) => next => action => {
   const state = getState();
-  console.log(action);
   if (action.type === tabTypes.CHANGE_ACTIVE_TAB) {
     if (action.payload === -1) {
       return next(tabOperations.changeActiveTab(state.tabs.list.length - 1));
