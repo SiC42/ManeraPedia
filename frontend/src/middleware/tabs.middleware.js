8f296e4de56e01a913d3153a0f0d93d18480dbe3
Simon Bordewisch <sbordewisch@gmx.de>
2019-11-08 15:26:03 +0100

    Add handling for removing last tab if it is selected

--- frontend/src/middleware/tabs.middleware.js
+++ frontend/src/middleware/tabs.middleware.js
@@ -1,10 +1,20 @@
 import { tabOperations, tabTypes } from "ducks/tabs";
 
-export default ({ getState }) => next => action => {
+export default ({ dispatch, getState }) => next => action => {
   const state = getState();
   if (action.type === tabTypes.CHANGE_ACTIVE_TAB) {
     if (action.payload === -1) {
       return next(tabOperations.changeActiveTab(state.tabs.list.length - 1));
+    }
+  }
+  if (action.type === tabTypes.REMOVE_TAB) {
+    const state = getState();
+    console.log(action);
+    console.log(state.tabs.list.length - 1);
+    if (action.payload === state.tabs.list.length - 1) {
+      console.log(action);
+      dispatch(tabOperations.changeActiveTab(state.tabs.list.length - 2));
+      return next(action);
     }
   }
   return next(action);
