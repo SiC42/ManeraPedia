f4cb583d58ebaeae50e0fe42fd2bd64301bcf272
Simon Bordewisch <sbordewisch@gmx.de>
2019-11-13 16:14:44 +0100

    Change wikiRefPlugin to blockPlugin

--- frontend/src/helpers/markdown/wikiRef/plugin.js
+++ frontend/src/helpers/markdown/wikiRef/plugin.js
@@ -5,14 +5,14 @@ const NODE_TYPE = "wikiRef";
 const START_SYMBOL = "{";
 const LINK_REGEX = /^\{\{(.+?)\}\}/;
 
-export default function wikiLinkPlugin() {
+export default function wikiRefPlugin() {
   const parser = this.Parser.prototype;
-  const { inlineTokenizers, inlineMethods } = parser;
-  inlineTokenizers[NODE_TYPE] = createInlineTokenizer({
+  const { blockTokenizers, blockMethods } = parser;
+  blockTokenizers[NODE_TYPE] = createInlineTokenizer({
     LINK_REGEX,
     START_SYMBOL
   });
-  inlineMethods.splice(inlineMethods.indexOf("reference"), 0, NODE_TYPE);
+  blockMethods.splice(blockMethods.indexOf("list"), 0, NODE_TYPE);
 
   // Stringify for wiki link
   const { Compiler } = this;
