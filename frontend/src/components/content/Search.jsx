10ca8c73196942bf60797ff30f9ea27a44c6c1de
Simon Bordewisch <sbordewisch@gmx.de>
2019-11-20 15:36:48 +0100

    Add rendering titles as links

--- frontend/src/components/content/Search.jsx
+++ frontend/src/components/content/Search.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Chip, Collapse, Link } from "@material-ui/core";
 import Divider from "@material-ui/core/Divider";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import { searchActions } from "ducks/search/";
 import React from "react";
-import { Chip, Collapse } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 import Markdown from "./Markdown";
 
 const useStyles = makeStyles(theme => ({
@@ -12,17 +15,33 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const renderChips = tags => (
-  <>
-    {tags.map((tag, i) => (
-      <Chip size="small" label={tag} key={tag} />
-    ))}
-  </>
-);
-
 export default function Search(props) {
   const { activeTabId, index, results, title } = props;
   const classes = useStyles();
+
+  const dispatch = useDispatch();
+
+  const fetchSelectedArticle = articleTitle => {
+    dispatch(
+      searchActions.getArticleRequest({
+        title: articleTitle,
+        focus: true,
+        location: "autosuggest"
+      })
+    );
+  };
+  const openNewTab = article => event => {
+    event.preventDefault();
+    fetchSelectedArticle(article);
+  };
+  const renderChips = tags => (
+    <>
+      {tags.map((tag, i) => (
+        <Chip size="small" label={tag} key={tag} />
+      ))}
+    </>
+  );
+
   return (
     <Paper
       id={`simple-tabpanel-${index}`}
@@ -34,16 +53,22 @@ export default function Search(props) {
         {title}
       </Typography>
       {results.map(result => (
-        <>
+        <div key={result.title}>
           <Typography variant="h5" component="h2">
-            {result.title}
+            <Link
+              href="#"
+              value={result.title}
+              onClick={openNewTab(result.title)}
+            >
+              {result.title}
+            </Link>
           </Typography>
           {renderChips(result.tags)}
           <Collapse collapsedHeight="40px">
             <Markdown source={result.text.slice(0, 500)} noRequests />
           </Collapse>
           <Divider />
-        </>
+        </div>
       ))}
     </Paper>
   );
